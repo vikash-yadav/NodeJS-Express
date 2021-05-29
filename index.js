1dbb5701e9b9842a61437066a001be96ad352b59
vikash-yadav <vikash.yadav9911@gmail.com>
2021-05-30 00:13:26 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ const express = require("express");
 
 const app = express();
 
-app.use("/test",(req, res)=>{
-    console.log("Recieved request"); 
-    res.status(200).send("recieved request 2");       
-});
+const usersRoutes = requre("../routes/users.route");
+
 
 app.listen(3000, ()=> {
     console.log("I am ready to listen you");
