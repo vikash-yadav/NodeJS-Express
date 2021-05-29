8dead0c245f917babbfa306bb259974da5de283b
vikash-yadav <vikash.yadav9911@gmail.com>
2021-05-29 23:38:54 +0530

    Initial Commit

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,25 +1,22 @@
 const { restart } = require("nodemon");
 const usersService = require("../services/users.service");
 
-exports.register = (req,res, next)=>{
-
-    const data = {
-        firstName: req.body.firstName ,
-        lastName: req.body.lastName,
-        emailId: req.body.emailId,
-        password: req.body.password,
-    };
-usersService.register(data, (error, results) => {
-    if(error) {
-        console.log(error)
-        return res.status(400).send({ sucess: 0, data: "Bad request"});
+exports.register = (req, res, next) => {
+  // Validation area
+  const data = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    emailId: req.body.emailId,
+    password: req.body.password,
+  };
+  usersService.register(data, (error, results) => {
+    if (error) {
+      console.log(error);
+      return res.status(400).send({ sucess: 0, data: "Bad request" });
     }
     return res.status(201).send({
-        sucess: 1,
-        data: results,
-    })
-});
-
-
+      sucess: 1,
+      data: results,
+    });
+  });
 };
-
