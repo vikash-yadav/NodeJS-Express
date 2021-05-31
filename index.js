5d02791cdc0ac7528ef503bc8b9860bbb7d2bc8a
vikash-yadav <vikash.yadav9911@gmail.com>
2021-05-31 23:15:12 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require("express");
 
 const app = express();
-
+const bodyParser = require("body-parser");
 const usersRoutes = requre("../routes/users.route");
 
-app.user("/users",usersRoutes);
+app.user("/users", usersRoutes);
 
-
-app.listen(3000, ()=> {
-    console.log("I am ready to listen you");
+app.listen(3000, () => {
+  console.log("I am ready to listen you");
 });
