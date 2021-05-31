e5cd3c949e2e24a34660b8348f793b7644b4bb8d
vikash-yadav <vikash.yadav9911@gmail.com>
2021-05-31 23:28:50 +0530

    Initial Commit

--- routes/users.route.js
+++ routes/users.route.js
@@ -5,6 +5,6 @@ var express = require("express");
 var router = express.Router();
 
 router.post("/register", usersController.register);
-router.post("/login") , usersController.login);
+router.post("/login" , usersController.login);
 
 module.exports = router;
